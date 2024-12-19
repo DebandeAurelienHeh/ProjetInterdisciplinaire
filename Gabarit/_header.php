372b4bcf9a3dd8aef30c2a96a068b86658432d78
DebandeAurelienHeh <aurelien.debande@std.heh.be>
2024-12-19 14:46:16 +0100

    v1.3.1 css sur le header et changement des redirections boutons

--- Gabarit/_header.php
+++ Gabarit/_header.php
@@ -1,6 +1,22 @@
-<?php
-?>
-
-<header>
-    <h1>Header A CHANGER</h1>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../Horaires/horairesIndex.php">Horaires</a></li>
+                <li><a href="../Inscriptions/inscriptionsIndex.php">Inscriptions</a></li>
+                <li><a href="../Ressources/ressourcesIndex.php">Ressources</a></li>
+            </ul>
+        </nav>
+        <a class="connexion" href="./connexion_page">Connexion</a>
+    </header>
+</body>
+</html>
